66338444b97ca01ab52e92ab282126836a4fcc31
tirumalateja19 <tirumalateja.jampani@gmail.com>
2026-07-09 19:47:02 +0530

    Initializing Project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-5xl font-serif mt-5 text-center text-green-400">
+        Courier App
+      </h1>
+    </div>
+  );
+};
+
+export default App;
